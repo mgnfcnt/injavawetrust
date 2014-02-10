42f87b0ed3951da239fae44521ecbfad750c4af3
Volkan Ozcelik <volkan@o2js.com>
2014-02-09 16:13:56 -0800

    moved the file to the proper folder.

--- examples/sendmail/SendEmailJavaApi.java
+++ examples/sendmail/SendEmailJavaApi.java
@@ -1,5 +1,12 @@
 package com.alican.akkus.eposta.yolla;
 
+/*
+ * Java'daki Mail Api'isyle e-posta yolluyoruz .
+ * Gerekli kütüphanenin güncel sürümünü burdan;
+ * http://www.oracle.com/technetwork/java/javamail/index-138643.html
+ * indirip projenize ekledikten sonra kullanabilirsiniz.
+ */
+
 import java.util.Properties;
 
 import javax.mail.Authenticator;
